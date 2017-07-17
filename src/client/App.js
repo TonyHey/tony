5c2c97bdce4ad7af658ae7d4173706c7b3e7804c
tony <562359635@qq.com>
2017-07-18 01:14:13 +0800

    add silk

--- src/client/App.js
+++ src/client/App.js
@@ -1,17 +1,67 @@
-import React from "react"
+import React, { Component } from "react"
 import logo from "../logo.svg"
 import styles from "./App.scss"
 
-const App = () => (
-    <div className={styles.app}>
-        <div className={styles.app_header}>
-            <img src={logo} className={styles.app_logo} alt="logo" />
-            <h2>Welcome to React</h2>
-        </div>
-        <p className={styles.app_intro}>
-            To get started, edit <code>src/App.js</code> and save to reload
-        </p>
-    </div>
-)
+class App extends Component {
+    componentDidMount() {
+        /* eslint-disable */
+        let c = document.getElementsByTagName("canvas")[0],
+            x = c.getContext("2d"),
+            pr = window.devicePixelRatio || 1,
+            w = window.innerWidth,
+            h = window.innerHeight,
+            f = 90,
+            m = Math,
+            r = 0,
+            u = m.PI * 2,
+            v = m.cos,
+            z = m.random
+        let q
+        c.width = w*pr
+        c.height = h*pr
+        x.scale(pr, pr)
+        x.globalAlpha = 0.6
+        function i(){
+            x.clearRect(0,0,w,h)
+            q=[{x:0,y:h*.7+f},{x:0,y:h*.7-f}]
+            while(q[1].x<w+f) d(q[0], q[1])
+        }
+        function d(i,j){
+            x.beginPath()
+            x.moveTo(i.x, i.y)
+            x.lineTo(j.x, j.y)
+            var k = j.x + (z()*2-0.25)*f,
+                n = y(j.y)
+            x.lineTo(k, n)
+            x.closePath()
+            r-=u/-50
+            x.fillStyle = "#"+(v(r)*127+128<<16 | v(r+u/3)*127+128<<8 | v(r+u/3*2)*127+128).toString(16)
+            x.fill()
+            q[0] = q[1]
+            q[1] = {x:k,y:n}
+        }
+        function y(p){
+            var t = p + (z()*2-1.1)*f
+            return (t>h||t<0) ? y(p) : t
+        }
+        document.addEventListener("click", () => i())
+        i()
+        /* eslint-enable */
+    }
+    render() {
+        return (
+            <div className={styles.app}>
+                <div className={styles.app_header}>
+                    <img src={logo} className={styles.app_logo} alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className={styles.app_intro}>
+                    To get started, edit <code>src/App.js</code> and save to reload
+                </p>
+                <canvas id="canvas_box" className={styles.canvas}>your browser not suport canvas</canvas>
+            </div>
+        )
+    }
+}
 
 export default App
