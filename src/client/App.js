38e63904dbf0b436c79fc6a7e2651b946319c4b0
tony <562359635@qq.com>
2017-08-05 13:05:09 +0800

    update ready

--- src/client/App.js
+++ src/client/App.js
@@ -60,19 +60,22 @@ class App extends Component {
                 signature: data.signature,
                 jsApiList: ["onMenuShareAppMessage"] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             })
-            wx.onMenuShareAppMessage({
-                title: "Sharing title test", // 分享标题
-                desc: "Sharing description test", // 分享描述
-                link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-                imgUrl: "https://upload.freedomlove.me/upload/motor.jpg", // 分享图标
-                success() {
-                    console.log("success")
-                    // 用户确认分享后执行的回调函数
-                },
-                cancel() {
-                    console.log("failed")
-                    // 用户取消分享后执行的回调函数
-                }
+            wx.ready(() => {
+                alert("ready to share")
+                wx.onMenuShareAppMessage({
+                    title: "Sharing title test", // 分享标题
+                    desc: "Sharing description test", // 分享描述
+                    link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: "https://upload.freedomlove.me/upload/motor.jpg", // 分享图标
+                    success() {
+                        console.log("success")
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel() {
+                        console.log("failed")
+                        // 用户取消分享后执行的回调函数
+                    }
+                })
             })
         }).catch(err => console.log(err))
     }
