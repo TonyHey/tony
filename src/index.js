8cfb9bff0f8f8500f2444712d3b277005eb1779d
Tony <562359635@qq.com>
2017-08-16 13:39:43 +0800

    add google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./client/App"
 // import registerServiceWorker from "./registerServiceWorker"
+/* eslint-disable */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-104693538-1', 'auto');
+ga('send', 'pageview');
+ /* eslint-enable */
 
 ReactDOM.render(<App />, document.getElementById("root"))
 // registerServiceWorker()
