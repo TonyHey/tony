1acf6014dc6eb8280535670045714a38496c47ca
Tony <562359635@qq.com>
2017-12-07 15:53:34 +0800

    update router

--- src/client/routes/index.js
+++ src/client/routes/index.js
@@ -1,26 +1,26 @@
 import React from "react"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 // import { CSSTransitionGroup } from "react-transition-group"
-import Bundle from "./bundle"
+// import Bundle from "./bundle"
 // import styles from "./router.scss"
-// import App from "../container/App"
-// import Login from "../pages/login"
+import App from "../container/home"
+import Test from "../container/test"
 
 
-const App = props => (
-    <Bundle load={() => import("../container/home")}>
-        {Component => <Component {...props} />}
-    </Bundle>
-)
-const Test = props => (
-    <Bundle load={() => import("../container/test")}>
-        {Component => <Component {...props} />}
-    </Bundle>
-)
+// const App = props => (
+//     <Bundle load={() => import("../container/home")}>
+//         {Component => <Component {...props} />}
+//     </Bundle>
+// )
+// const Test = props => (
+//     <Bundle load={() => import("../container/test")}>
+//         {Component => <Component {...props} />}
+//     </Bundle>
+// )
 const routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route location={location} key={`${location.key}_0`} path="/login" component={Test} />
+            <Route location={location} key={`${location.key}_0`} path="/test" component={Test} />
             <Route location={location} key={`${location.key}_1`} path="/" component={App} />
         </Switch>
     </BrowserRouter>
